6932478dbf5c1ef42de6d64d4cd91eb564f75c9c
amitrke <amitrke@gmail.com>
2022-06-25 17:10:49 +0000

    Initial code

---
+++ src/students.py
@@ -0,0 +1,67 @@
+studentList = [
+	{
+  	'name': "John",
+    'marks': [
+    	{
+      	'subject': 'science',
+        'marks': 45
+      },
+      {
+      	'subject': 'maths',
+        'marks': 66
+      }
+    ]
+  },
+  {
+  	'name': "Mike",
+    'marks': [
+    	{
+      	'subject': 'science',
+        'marks': 34
+      },
+      {
+      	'subject': 'maths',
+        'marks': 72
+      }
+    ]
+  },
+  {
+  	'name': "Nancy",
+    'marks': [
+    	{
+      	'subject': 'science',
+        'marks': 54
+      },
+      {
+      	'subject': 'maths',
+        'marks': 51
+      }
+    ]
+  },
+  {
+  	'name': "Rohan",
+    'marks': [
+    	{
+      	'subject': 'science',
+        'marks': 90
+      },
+      {
+      	'subject': 'maths',
+        'marks': 11
+      }
+    ]
+  },
+  {
+  	'name': "Kelly",
+    'marks': [
+    	{
+      	'subject': 'science',
+        'marks': 66
+      },
+      {
+      	'subject': 'maths',
+        'marks': 34
+      }
+    ]
+  }
+]
