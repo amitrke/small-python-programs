cd10a483752451b9d7197a149f871d9dc62bc931
Amit Kumar <amitrke@gmail.com>
2022-07-02 10:11:06 -0700

    added mult table code

---
+++ src/multTable.py
@@ -0,0 +1,10 @@
+def printTable(inputNum):
+    #Should print something like below if the inputNum is 2
+    #
+    #2 X 1 = 2
+    #2 X 2 = 4
+    # ..
+    #2 X 10 = 20
+    print("Test")
+
+printTable(2)
