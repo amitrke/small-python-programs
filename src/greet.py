85b150b204c5af29bf0da48fb9abaa61dda27701
Amit Kumar <amitrke@gmail.com>
2022-07-06 17:00:10 -0700

    Added greet function

---
+++ src/greet.py
@@ -0,0 +1,5 @@
+def greet(name):
+    print("Test")
+    #Should print: "Hello John!" if the input to the function is John
+
+greet("John")
--- src/multTable.py
+++ src/multTable.py
@@ -1,3 +1,12 @@
+def printTableRow(inputNum, rowNum):
+    #Should print something like below if the inputNum is 2
+    #
+    #2 X 1 = 2
+    #we get 2 from the inputNum
+    #we get 1 from rowNum
+    #2 on the right hand side of = is the product of inputNum and rowNum
+    print("Test")
+
 def printTable(inputNum):
     #Should print something like below if the inputNum is 2
     #
@@ -7,4 +16,5 @@ def printTable(inputNum):
     #2 X 10 = 20
     print("Test")
 
+printTableRow(2, 1)
 printTable(2)
